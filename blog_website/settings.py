6d6516c7db4959a98ad1624e14878cb2cb3ecf58
piriyaraj <piriyaraj1998@gmail.com>
2022-10-25 14:44:49 +0530

    update

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'user_profile',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'user_profile',
     'notification',
     'ckeditor',
     'Extract',
